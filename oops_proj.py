68f7ceeca212d09878ef20dd480c73733548e6b9
Yash <yash27bhatewara@gmail.com>
2025-10-17 14:57:53 +0530

    project -basic structure ready

---
+++ oops_proj.py
@@ -0,0 +1,28 @@
+class Mergebook:
+    def __init__(self):
+        self.username = ''
+        self.password = ''
+        self.loffedin = False
+        self.menu()
+
+
+
+    def menu(self):
+        user_input = input("""Welcome to Mergebook!! How you would like to proceed?"
+                        1. Press 1 to signup
+                        2. Press 2 to signin
+                        3. Press 3 to write a post
+                        4. Press 4 to message a friend
+                        5. Press any other key to exit""")
+        if user_input == "1":
+            pass
+        elif user_input == "2":
+            pass
+        elif user_input == "3":
+            pass
+        elif user_input =="4":
+            pass
+        else:
+            exit()
+
+obj = Mergebook()
