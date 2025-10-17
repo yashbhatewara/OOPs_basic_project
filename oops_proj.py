65a492c739b84ed502a475a6992007d082d66077
Yash <yash27bhatewara@gmail.com>
2025-10-17 17:13:14 +0530

    Project completed

--- oops_proj.py
+++ oops_proj.py
@@ -13,15 +13,17 @@ class Mergebook:
                         2. Press 2 to signin
                         3. Press 3 to write a post
                         4. Press 4 to message a friend
-                        5. Press any other key to exit""")
+                        5. Press any other key to exit
+                           
+                           -> """)
         if user_input == "1":
             self.signup()
         elif user_input == "2":
             self.signin()
         elif user_input == "3":
-            pass
+            self.my_post()
         elif user_input =="4":
-            pass
+            self.sendmsg()
         else:
             exit()
 
@@ -45,10 +47,27 @@ class Mergebook:
                 print("You have successfully signedin !!")
                 self.loggedin = True
             else:
-                print("Please enter the credentials..")
+                print("Please enter the correct credentials..")
         print("\n")
         self.menu()
 
+    def my_post(self):
+        if self.loggedin == True:
+            txt = input("Enter your message here -> ")
+            print(f"Following content has been posted -> {txt}")
+        else:
+            print("You need to signin first to post something...")
+        print("\n") 
+        self.menu()
 
+    def sendmsg(self):
+        if self.loggedin==True:
+            txt = input("Enter your message here -> ")
+            frnd = input("Whom to send the msg? -> ")
+            print(f"Your message has been sent to {frnd}")
+        else:
+            print("You need to signin first to post something...")
+        print("\n")
+        self.menu()
 
 obj = Mergebook()
